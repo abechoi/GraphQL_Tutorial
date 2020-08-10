ffcd63753881d11e33a6006e96033918e263bd8b
abechoi <abraham.choi@icloud.com>
2020-08-09 17:01:11 -0700

    Connected to database

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const dbURI = 'mongodb+srv://abechoi:iwbiny319@cluster0.eoe0p.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log('Connected to database...'); 
+    app.listen(4000, () => {
+      console.log('Listening to port:4000...');
+    });
+})
+.catch((err) => console.log(err));
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
-
-app.listen(4000, () => {
-  console.log('Listening to port:4000...');
-});
